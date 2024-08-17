6c020eb3204f90131f2ee80f58beb92b0de4d964
Sanghyeok Park <ray5273@naver.com>
2024-08-17 23:07:09 +0900

    Temporary removed locale files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,10 +85,10 @@ const config: Config = {
     "docusaurus-theme-openapi-docs",
     '@docusaurus/theme-mermaid',
   ],
-  i18n: {
-    defaultLocale: 'ko',
-    locales: ['en', 'ko'],
-  },
+  // i18n: {
+  //   defaultLocale: 'ko',
+  //   locales: ['en', 'ko'],
+  // },
   markdown: {
     mermaid: true,
   },
